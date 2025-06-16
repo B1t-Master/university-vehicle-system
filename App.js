8a33f67c00bdb30dae491f0c9b8116c098a76096
wesgitley <wesley.mutisya@strathmore.edu>
2025-06-16 11:05:55 +0300

    Sign Up text

--- App.js
+++ App.js
@@ -3,11 +3,11 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <div>
+       <LoginSignup></LoginSignup>
+    </div>
+    
+  )
 }
 
 const styles = StyleSheet.create({
