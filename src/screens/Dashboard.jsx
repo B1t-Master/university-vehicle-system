b0076e84f21609ddbd08e7ff486cc160b797553b
B1t-Master <isaac.njuguna@strathmore.edu>
2025-06-26 08:55:22 +0300

    Rearrange components on Dashboard

--- src/screens/Dashboard.jsx
+++ src/screens/Dashboard.jsx
@@ -1,38 +1,45 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
-import { useNavigation } from '@react-navigation/core';
-import { auth } from '../firebase';
+import React, { useState, useEffect } from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { useNavigation } from "@react-navigation/core";
+import { auth } from "../firebase";
+import { signOut } from "firebase/auth";
 
 const Dashboard = () => {
   const navigation = useNavigation();
   const [user, setUser] = useState(null);
 
-  useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged(user => {
-      if (user) {
-        setUser(user);
-      } else {
-        setUser(null);
-      }
-    });
-
-    return unsubscribe;
-  }, []);
+  const handleSignOut = () => {
+    auth
+      .signOut()
+      .then(() => {
+        navigation.replace("login");
+      })
+      .catch((error) => alert(error.message));
+  };
 
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Welcome to the Dashboard!</Text>
-      {user && (
+      <View style={[styles.topBar]}>
+        <TouchableOpacity
+          style={[styles.button, styles.signOut]}
+          onPress={handleSignOut}
+        >
+          <Text style={styles.buttonText}>Sign out</Text>
+        </TouchableOpacity>
+      </View>
+      <Text style={styles.text}>
+        Welcome {auth.currentUser?.email.split("@")[0]}
+      </Text>
+      {/* {user && (
         <View style={styles.profileContainer}>
           <Text style={styles.profileName}>{user.email}</Text>
-          <Text style={styles.profileEmail}>{user.email}</Text>
         </View>
-      )}
-<TouchableOpacity
-        style={styles.addVehicleButton}
-        onPress={() => navigation.navigate('AddVehicle')}
+      )} */}
+      <TouchableOpacity
+        style={[styles.button, styles.addVehicle]}
+        onPress={() => navigation.navigate("AddVehicle")}
       >
-        <Text style={styles.addVehicleButtonText}>Add Vehicle</Text>
+        <Text style={styles.buttonText}>Add Vehicle</Text>
       </TouchableOpacity>
     </View>
   );
@@ -40,42 +47,56 @@ const Dashboard = () => {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'flex-start',
-    alignItems: 'flex-start',
+    display: "flex",
+    // flex: 1,
+    justifyContent: "centre",
+    // alignItems: "centre",
     padding: 20,
+    // margin: 20,
   },
   text: {
     fontSize: 20,
-    fontWeight: 'bold',
-    textAlign: 'left',
+    fontWeight: "bold",
+    textAlign: "center",
+    marginTop: "10",
   },
   profileContainer: {
-    backgroundColor: '#f0f0f0',
+    backgroundColor: "#f0f0f0",
     padding: 20,
     borderRadius: 10,
     marginTop: 20,
   },
+  topBar: {
+    flexDirection: "row",
+    justifyContent: "flex-end",
+  },
   profileName: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 5,
   },
   profileEmail: {
     fontSize: 16,
   },
-  addVehicleButton: {
-    backgroundColor: '#0782F9',
-    width: '60%',
-    padding: 15,
+  button: {
+    backgroundColor: "#0782F9",
+    paddingHorizontal: 20,
+    paddingVertical: 10,
     borderRadius: 10,
-    alignItems: 'center',
-    marginTop: 40,
+    alignItems: "center",
+    margin: 10,
   },
-  addVehicleButtonText: {
-    color: 'white',
+  buttonText: {
+    color: "white",
+    fontWeight: "700",
     fontSize: 16,
-    fontWeight: 'bold',
+  },
+  signOut: {
+    alignSelf: "flex-end",
+  },
+  addVehicle: {
+    alignSelf: "centre",
+    marginTop: "40",
   },
 });
 
